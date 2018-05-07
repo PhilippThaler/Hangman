31e0f849bf5e65257f3165fda0f9aa6731700282
Philipp <thalerphilipp1@gmail.com>
2018-05-07 09:10:53 +0200

    Added 2 lines to restart the game when finished

--- Hangman.java
+++ Hangman.java
@@ -33,52 +33,63 @@ public class Hangman {
             System.out.println("Closing the program...");
             System.exit(1);
         }
-        // Grabbing a random word
-        String word = words.get(ThreadLocalRandom.current().nextInt(words.size()));
 
-        // char array for comparing the user input against the word
-        char[] guess = new char[word.length()];
-        Arrays.fill(guess, '_');
+        boolean playing = true;
+        while (playing) {// Grabbing a random word
+            String word = words.get(ThreadLocalRandom.current().nextInt(words.size()));
 
-        int wrongGuesses = 0;
-        scanner = new Scanner(System.in);
-        boolean isRight;
-        char input;
-        ArrayList<Character> usedChars = new ArrayList<>();
+            // char array for comparing the user input against the word
+            char[] guess = new char[word.length()];
+            Arrays.fill(guess, '_');
 
-        while (wrongGuesses < Drawing.stages.length - 1 && !word.equals(new String(guess))) {
-            isRight = false;
-            System.out.print("Word: ");
-            System.out.println(guess);
+            int wrongGuesses = 0;
+            scanner = new Scanner(System.in);
+            boolean isRight;
+            char input;
+            ArrayList<Character> usedChars = new ArrayList<>();
 
-            input = input();
-
-            // Checks for already used letters
-            while (usedChars.contains(input)) {
-                System.out.println("You already used this letter!");
+            while (wrongGuesses < Drawing.stages.length && !word.equals(new String(guess))) {
+                isRight = false;
                 System.out.print("Word: ");
                 System.out.println(guess);
-                input = input();
-            }
-            usedChars.add(input);
 
-            // If the guess was right, make the letters visible
-            for (int i = 0; i < word.length(); i++) {
-                if (input == word.charAt(i)) {
-                    guess[i] = input;
-                    isRight = true;
+                input = input();
+
+                // Checks for already used letters
+                while (usedChars.contains(input)) {
+                    System.out.println("You already used this letter!");
+                    System.out.print("Word: ");
+                    System.out.println(guess);
+                    input = input();
+                }
+                usedChars.add(input);
+
+                // If the guess was right, make the letters visible
+                for (int i = 0; i < word.length(); i++) {
+                    if (input == word.charAt(i)) {
+                        guess[i] = input;
+                        isRight = true;
+                    }
+                }
+
+                // Prints the hanged man
+                System.out.println(Drawing.stages[wrongGuesses]);
+
+                if (!isRight) {
+                    System.out.println("Wrong guess!");
+                    wrongGuesses++;
                 }
             }
+            System.out.println(
+                    (word.equals(new String(guess))) ? word + "\nYou won!" : "You lost, the word was: " + word);
 
-            // Prints the hanged man
-            System.out.println(Drawing.stages[wrongGuesses]);
-
-            if (!isRight) {
-                System.out.println("Wrong guess!");
-                wrongGuesses++;
+            System.out.println("Do you want to play again? [Y/N]");
+            if (scanner.next().toUpperCase().charAt(0) == 'Y') {
+                playing = true;
+            } else {
+                playing = false;
             }
         }
-        System.out.println((word.equals(new String(guess))) ? word + "\nYou won!" : "You lost, the word was: " + word);
     }
 
     // Method for handling input
